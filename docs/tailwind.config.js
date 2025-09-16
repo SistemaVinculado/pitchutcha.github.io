bab53a2c979d6fc4da5b73c08602b6137ae10289
SistemaVinculado <anapaulacorreiamoraes1983@gmail.com>
2025-09-15 22:40:23 -0300

    Create tailwind.config.js

---
+++ docs/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './_layouts/*.html',
+    './_includes/*.html',
+    './_posts/*.md',
+    './_articles/*.md'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
